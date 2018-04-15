dcbc27a380041f1147bc5a9b2fd8130e40ae43ff
Sean McElhare <sm860129@wcupa.edu>
2018-04-14 23:30:33 -0400

    changing login string

--- _settings.php
+++ _settings.php
@@ -4,20 +4,22 @@ class _settings
     /********************************************************************************************************/
     /** AUTHENTICATION CONSTANTS */
     /** ntopng Login URL Path */
-    const AUTH_URL = "http://localhost:3000/lua/login.lua?referer=localhost:3000/";
+    const AUTH_URL = "http://localhost:3000/lua/login.lua?";
 
     /** Default Login Credentials for ntopng */
     /**
     const AUTH_USERNAME = "admin";
     const AUTH_PASSWORD = "admin";
     */
-    const AUTH_USERNAME = "admin";
-    const AUTH_PASSWORD = "sean4132";
+
+    const AUTH_USERNAME = "ntopnglogger";
+    const AUTH_PASSWORD = "ntopnglogger!";
 
     /** Login Form Elements */
     const AUTH_USERNAME_INPUT = "_username";
     const AUTH_PASSWORD_INPUT = "password";
-    const AUTH_LOGIN_STRING = self::AUTH_USERNAME_INPUT."=".self::AUTH_USERNAME."&".self::AUTH_PASSWORD_INPUT
+    const AUTH_CLIENT_REFERER = "referer=localhost:3000";
+    const AUTH_LOGIN_STRING = self::AUTH_CLIENT_REFERER."&".self::AUTH_USERNAME_INPUT."=".self::AUTH_USERNAME."&".self::AUTH_PASSWORD_INPUT."=".self::AUTH_PASSWORD."/";
 
     const AUTH_SESSION_STORE = "./session_store/";
     const AUTH_SESSION_STORE_FILE = "session.txt";
